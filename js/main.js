a9363658095eacb8b8ab954f1db0806ca2dbfe3d
maxaakre <maxaakre@gmail.com>
2019-11-04 13:32:57 +0100

    lightbox

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@
 
 const form = document.querySelector('form'); // tar formulär
 
+
 searchFunction = e => {
     e.preventDefault(); // Förhindrar att formuläret uppdaterar sidan
     let inputValue = document.querySelector('input').value; // Tar emot värdet från input
     let select = document.querySelector('select').value; //Tar värdet från varje alternativ innuti select
-    getImages(inputValue, select);
+    removeContent();
+    getImages(inputValue, select); //samlar ihop värdet som man sedan nedanför 
 }
 
 getImages = (inputValue, select) => {
+    document.querySelector('.close-box').addEventListener('click', () => { //close kryset för att stänga ner lightbox
+        document.querySelector('#light-box').style.display = 'none';// =display none
+    })
 
-    let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=a02e608cfb3c9ba63718ff52f8215856&tags=${inputValue}&per_page=${select}&format=json&nojsoncallback=1`; // en fetch med Vilket sökord som användaren skriver in
+
+    let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f0ba267d7ad25a32cb67fab3494fbc83&tags=${inputValue}&per_page=${select}&format=json&nojsoncallback=1`; // en fetch med Vilket sökord som användaren skriver in
 
     fetch(url)
         .then(res => res.json()) //Gör om till json format
@@ -26,7 +32,21 @@ getImages = (inputValue, select) => {
     
                 `;
 
+
+
+
                 document.getElementById("output").innerHTML += output;
+
+
+
+                const imageArray = document.querySelectorAll("#output img")
+                imageArray.forEach(img => img.addEventListener('click', handleLightbox))
+                function handleLightbox(e) {
+                    let lightboxImage = document.querySelector('#lightbox-image');
+                    let newSource = e.target.getAttribute('src') // tar source länken från bilden som klickas
+                    lightboxImage.setAttribute('src', newSource) // sätter src attributen till bilden innanför lightboxen
+                    document.querySelector('#light-box').style.display = 'flex'; // visar bilden i lightboxen
+                }
             });
 
 
@@ -34,5 +54,9 @@ getImages = (inputValue, select) => {
 }
 
 
+let removeContent = () => {
+    document.getElementById("output").innerHTML = "";
+    document.querySelector("input").value = "";
+};
 
 form.addEventListener('submit', searchFunction) // anger en event till formuläret
